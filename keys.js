15240a3608a341bb958d8010091d87e2d7279e4d
Ricardo <ricardoalmeida312@gmail.com>
2023-03-27 11:01:36 +0100

    first commit

---
+++ keys.js
@@ -0,0 +1,5 @@
+//Access key: yd9O_Sz_cb5CupxVepWBicgm0YOi6bl995fAk9n1kcs
+//Secret Key:rtHNczaBNDvkLXuXncxM_pqUBUtnViw7eE47vtPb1Tc
+//https://api.unsplash.com/
+//Header:Authorization: Client-ID YOUR_ACCESS_KEY GET /search/photos
+//param: query
---
+++ src/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const searchImages = async (term) => {
+  const response = await axios.get("https://api.unsplash.com/search/photos", {
+    headers: {
+      Authorization: "Client- ID yd9O_Sz_cb5CupxVepWBicgm0YOi6bl995fAk9n1kcs",
+    },
+    params: {
+      query: term,
+    },
+  });
+  return response.data.results;
+};
+
+export default searchImages;
