3d18c15eae47a689db1db74c4e17f14ca36b639d
Ricardo <ricardoalmeida312@gmail.com>
2023-03-27 11:43:34 +0100

    second commit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,22 @@
+import { useState } from "react";
+
 const SearchBar = ({ onSubmit }) => {
-  const handleClick=()=>{
-    onSubmit('cars');
+  const [term, setTerm] = useState("");
+  const handleFormSubmit = (event) => {
+    onSubmit("asdf");
   };
-    return (
+
+  const handleChange = (event) => {
+    setTerm(event.target.value)
+  };
+
+  return (
     <div>
-        <input/>
-        <button onClick={handleClick}>Submit</button>
+      <form onSubmit={handleFormSubmit}>
+        <input value={term} onChange={handleChange} />
+      </form>
     </div>
-  )
+  );
 };
 
 export default SearchBar;
